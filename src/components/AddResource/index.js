d273a22c853e328c6abc88bb4dcd3af670c4fc16
Mahesh Srisailam <srisailammahesh77@gmail.com>
2022-07-30 16:59:28 +0000

    Updated

--- src/components/AddResource/index.js
+++ src/components/AddResource/index.js
@@ -1,8 +1,182 @@
 import {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {v4 as uuidv4} from 'uuid'
+import {
+  AddResourceHomeContainer,
+  AddResourceResponsiveContainer,
+  CardDetailsFillContainer,
+  UserContainer,
+  FormContainer,
+  Heading,
+  FormElement,
+  LabelElement,
+  InputElement,
+  BreakElement,
+  CreateButton,
+  ImageContainer,
+  Image,
+  UserTextButton,
+  NameElementError,
+  LinkElementError,
+  DescriptionElementError,
+  ButtonContainer,
+} from './styledComponents'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
 
 class AddResource extends Component {
+  state = {
+    addResourceList: [],
+    apiStatus: apiStatusConstants.initial,
+    userInputName: '',
+    userInputLink: '',
+    userInputDescription: '',
+    isEmptyText: false,
+  }
+
+  componentDidMount() {
+    this.getResourceData()
+  }
+
+  onChangeName = event => {
+    this.setState({userInputName: event.target.value})
+  }
+
+  onChangeLink = event => {
+    this.setState({userInputLink: event.target.value})
+  }
+
+  onChangeDescription = event => {
+    this.setState({userInputDescription: event.target.value})
+  }
+
+  onClickSubmit = event => {
+    event.preventDefault()
+    const {userInputName, userInputLink, userInputDescription} = this.state
+    let newResource = null
+    if (
+      userInputName.length === 0 ||
+      userInputLink.length === 0 ||
+      userInputDescription.length === 0
+    ) {
+      this.setState(prevState => ({isEmptyText: !prevState.isEmptyText}))
+    } else {
+      newResource = {
+        id: uuidv4(),
+        name: userInputName,
+        link: userInputLink,
+        description: userInputDescription,
+      }
+      this.setState(prevState => ({
+        addResourceList: [...prevState.addResourceList, newResource],
+        userInputName: '',
+        userInputLink: '',
+        userInputDescription: '',
+      }))
+    }
+  }
+
+  getResourceData = async () => {
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+    const addResourceUrl =
+      'https://media-content.ccbp.in/website/react-assignment/add_resource.json'
+    const response = await fetch(addResourceUrl)
+    if (response.ok) {
+      const fetchedData = await response.json()
+      this.setState({
+        addResourcesList: fetchedData,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({
+        apiStatus: apiStatusConstants.failure,
+      })
+    }
+  }
+
   render() {
-    return <div className="add-resource-main-container">Add Resources</div>
+    const {
+      userInputName,
+      userInputLink,
+      userInputDescription,
+      isEmptyText,
+      addResourceList,
+    } = this.state
+    console.log(addResourceList)
+    return (
+      <AddResourceHomeContainer>
+        <AddResourceResponsiveContainer>
+          <CardDetailsFillContainer>
+            <UserContainer>
+              <Link to="/">
+                <UserTextButton type="button">&lt; User</UserTextButton>
+              </Link>
+            </UserContainer>
+            <FormContainer>
+              <Heading>Add a Resource</Heading>
+
+              <FormElement onSubmit={this.onClickSubmit}>
+                <LabelElement htmlFor="name">NAME</LabelElement>
+                <BreakElement />
+                <InputElement
+                  type="text"
+                  id="name"
+                  value={userInputName}
+                  onChange={this.onChangeName}
+                  placeholder="Enter the Name"
+                />
+                <BreakElement />
+                {isEmptyText && userInputName.length === 0 && (
+                  <NameElementError>*Name is required</NameElementError>
+                )}
+                <LabelElement htmlFor="link">LINK</LabelElement>
+                <BreakElement />
+                <InputElement
+                  type="text"
+                  id="link"
+                  value={userInputLink}
+                  onChange={this.onChangeLink}
+                  placeholder="Enter the Link"
+                />
+                <BreakElement />
+                {isEmptyText && userInputLink.length === 0 && (
+                  <LinkElementError>*Link is required</LinkElementError>
+                )}
+                <LabelElement htmlFor="description">DESCRIPTION</LabelElement>
+                <BreakElement />
+                <InputElement
+                  type="text"
+                  id="description"
+                  value={userInputDescription}
+                  onChange={this.onChangeDescription}
+                  placeholder="Enter the description"
+                />
+                <BreakElement />
+                {isEmptyText && userInputDescription.length === 0 && (
+                  <DescriptionElementError>
+                    *Description is required
+                  </DescriptionElementError>
+                )}
+                <ButtonContainer>
+                  <CreateButton type="submit">Create</CreateButton>
+                </ButtonContainer>
+              </FormElement>
+            </FormContainer>
+          </CardDetailsFillContainer>
+          <ImageContainer>
+            <Image
+              src="https://res.cloudinary.com/diocftr6t/image/upload/v1659184818/image_9_gdviru.png"
+              alt="image"
+            />
+          </ImageContainer>
+        </AddResourceResponsiveContainer>
+      </AddResourceHomeContainer>
+    )
   }
 }
 
---
+++ src/components/AddResource/styledComponents.js
@@ -0,0 +1,113 @@
+import styled from 'styled-components'
+
+export const AddResourceHomeContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #fbfbfb;
+`
+
+export const AddResourceResponsiveContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+export const CardDetailsFillContainer = styled.div`
+  height: 100%;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 20px;
+`
+export const UserContainer = styled.div`
+  align-self: flex-start;
+  margin-bottom: 50px;
+`
+
+export const UserTextButton = styled.button`
+  font-family: 'Roboto';
+  font-size: 16px;
+  outline: none;
+  border: none;
+  background: transparent;
+`
+
+export const FormContainer = styled.div`
+  height: 50%;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Heading = styled.h1`
+  font-family: 'Roboto';
+  font-size: 24px;
+  color: #171f46;
+`
+
+export const FormElement = styled.form`
+  height: 100%;
+  width: 100%;
+`
+export const InputElement = styled.input`
+  height: 30px;
+  width: 100%;
+  outline: none;
+  cursor: pointer;
+  background: #ffffff;
+  padding-left: 10px;
+  border: 1px solid #d7dfe9;
+  border-radius: 2px;
+`
+export const BreakElement = styled.br``
+
+export const NameElementError = styled.p`
+  font-size: 14px;
+  color: red;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`
+export const LinkElementError = styled.p`
+  font-size: 14px;
+  color: red;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`
+export const DescriptionElementError = styled.p`
+  font-size: 14px;
+  color: red;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`
+export const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+`
+export const CreateButton = styled.button`
+  height: 30px;
+  width: 80px;
+  color: #ffffff;
+  background-color: #0b69ff;
+  border: none;
+  border-radius: 3px;
+`
+
+export const LabelElement = styled.label`
+  font-size: 14px;
+  padding-top: 16px;
+`
+export const ImageContainer = styled.div`
+  height: 100%;
+  width: 50%;
+`
+export const Image = styled.img`
+  height: 100%;
+  width: 100%;
+`
