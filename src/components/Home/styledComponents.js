640f188853a6b83165d786d81ca95d42790d3f4d
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-07-31 23:48:57 +0530

    Update styledComponents.js

--- src/components/Home/styledComponents.js
+++ src/components/Home/styledComponents.js
@@ -1,4 +1,3 @@
-
 import styled from 'styled-components'
 
 export const HomeMainContainer = styled.div`
@@ -29,6 +28,10 @@ export const TagListItem = styled.li``
 export const TagButton = styled.button`
   height: 30px;
   width: 150px;
+  outline: none;
+  border: 1px solid #bacad1;
+  color: ${props => (props.isActive ? '#ffffff' : '#171F46')};
+  background-color: ${props => (props.isActive ? '#0B69FF' : '#D7DFE9')};
 `
 
 export const SearchContainer = styled.div`
