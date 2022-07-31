82e8a9fcac462f1037a726f3a854e13796b23c4c
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-07-31 23:21:42 +0530

    Update index.js

--- src/components/ResourceCard/index.js
+++ src/components/ResourceCard/index.js
@@ -2,21 +2,24 @@ import {
   ResourcesCard,
   IconAndTitleContainer,
   CardIcon,
+  CardTitleCategoryContainer,
   CardTitle,
   CardDescription,
   CardLink,
+  CardCategory,
 } from './styledComponents'
 
 const ResourceCard = props => {
   const {resourceDetails} = props
-  const {title, link, description, iconUrl} = resourceDetails
+  const {title, link, description, category, iconUrl} = resourceDetails
   return (
     <ResourcesCard>
       <IconAndTitleContainer>
         <CardIcon src={iconUrl} alt="icon" />
-        <>
+        <CardTitleCategoryContainer>
           <CardTitle>{title}</CardTitle>
-        </>
+          <CardCategory>{category}</CardCategory>
+        </CardTitleCategoryContainer>
       </IconAndTitleContainer>
       <CardLink href={link}>{link}</CardLink>
       <CardDescription>{description}</CardDescription>
@@ -25,3 +28,4 @@ const ResourceCard = props => {
 }
 
 export default ResourceCard
+
