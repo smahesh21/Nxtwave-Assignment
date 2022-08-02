38cef6ad281207e924bbc025517a35c12b4024f7
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-08-02 19:26:14 +0530

    Update index.js

--- src/components/AddResource/index.js
+++ src/components/AddResource/index.js
@@ -1,4 +1,3 @@
-
 import {Component} from 'react'
 import {Link, Redirect} from 'react-router-dom'
 import Cookies from 'js-cookie'
@@ -50,6 +49,7 @@ class AddResource extends Component {
     userInputDescription: '',
     userInputImage: '',
     changeImage: false,
+    isSubmitted: false,
   }
 
   componentDidMount() {
@@ -100,6 +100,7 @@ class AddResource extends Component {
       userInputDescription,
       userInputImage,
     } = this.state
+    this.setState({isSubmitted: true})
     const isNewResourceLinkValid = this.isValidResourceLink()
     const isNewResourceDescriptionValid = this.isValidResourceDescription()
     let newResource
@@ -160,6 +161,7 @@ class AddResource extends Component {
       }
       this.setState(prevState => ({
         addResourceList: [...prevState.addResourceList, newResource],
+        isSubmitted: false,
         userInputName: '',
         userInputLink: '',
         userInputDescription: '',
@@ -200,6 +202,7 @@ class AddResource extends Component {
       userInputImage,
       addResourceList,
       changeImage,
+      isSubmitted,
     } = this.state
     const isNameEmpty = userInputName.length === 0
     const isLinkEmpty = userInputLink.length === 0
@@ -235,8 +238,10 @@ class AddResource extends Component {
                 placeholder="Enter the Name"
               />
               <BreakElement />
-              {isNameEmpty ? (
-                <ErrorMessageElement>Name is required*</ErrorMessageElement>
+              {isSubmitted && isNameEmpty ? (
+                <ErrorMessageElement>
+                  Valid name is required*
+                </ErrorMessageElement>
               ) : null}
               <LabelElement htmlFor="link">LINK</LabelElement>
               <BreakElement />
@@ -249,8 +254,10 @@ class AddResource extends Component {
                 placeholder="Enter the Link"
               />
               <BreakElement />
-              {isLinkEmpty || isLinkValid ? (
-                <ErrorMessageElement>Link is required*</ErrorMessageElement>
+              {isSubmitted && (isLinkEmpty || isLinkValid) ? (
+                <ErrorMessageElement>
+                  Valid link is required*
+                </ErrorMessageElement>
               ) : null}
 
               <LabelElement htmlFor="description">DESCRIPTION</LabelElement>
@@ -258,8 +265,6 @@ class AddResource extends Component {
               <DescriptionInputElement
                 type="text"
                 id="description"
-                rows="4"
-                cols="68"
                 value={userInputDescription}
                 onChange={this.onChangeDescription}
                 onBlur={this.onBlurDescription}
@@ -268,9 +273,9 @@ class AddResource extends Component {
                 {userInputDescription}
               </DescriptionInputElement>
               <BreakElement />
-              {isDescriptionEmpty || isValidDescription ? (
+              {isSubmitted && (isDescriptionEmpty || isValidDescription) ? (
                 <ErrorMessageElement>
-                  Description is required*
+                  Valid description is required*
                 </ErrorMessageElement>
               ) : null}
               <BreakElement />
@@ -287,8 +292,10 @@ class AddResource extends Component {
                   <BsUpload size={16} /> {changeImage ? 'Change' : 'Upload'}
                 </LabelElement>
               </ImageUploadContainer>
-              {isImageUrlEmpty && (
-                <ErrorMessageElement>Image is required*</ErrorMessageElement>
+              {isSubmitted && isImageUrlEmpty && (
+                <ErrorMessageElement>
+                  Valid image is required*
+                </ErrorMessageElement>
               )}
               <ButtonContainer>
                 <CreateButton type="submit">Create</CreateButton>
@@ -356,3 +363,4 @@ class AddResource extends Component {
 }
 
 export default AddResource
+
